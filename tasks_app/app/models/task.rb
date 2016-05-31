9e254e95fb34f4ed0d53559be833092112cb2b03
Braden Polly <bpolly6@gmail.com>
2016-05-31 12:07:33 -0400

    AJAX completed working. Also added font-awesome icons to click to check and uncheck status

--- tasks_app/app/models/task.rb
+++ tasks_app/app/models/task.rb
@@ -3,7 +3,11 @@ class Task < ActiveRecord::Base
   scope :incomplete, -> { where(complete: nil) }
 
   def mark_complete!
-    self.update_attribute(:complete, true)
+    if self.completed
+      self.update_attribute(:completed, false)
+    else
+      self.update_attribute(:completed, true)
+    end
   end
 
 end
