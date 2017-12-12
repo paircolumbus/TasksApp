55460faa99e282d88fba40a1b769e58f3223b0d2
Chris Kuck <fkuck@covermymeds.com>
2017-12-11 21:17:24 -0500

    added model, routes, and controller for tasks

--- Gemfile
+++ Gemfile
@@ -29,9 +29,11 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+
+gem 'rack-cors', :require => 'rack/cors'
+gem 'pry'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -48,6 +50,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'pry-rails'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,41 @@
 class TasksController < ApplicationController
+  
+  def index
+    @tasks = Task.all
+    render json: @tasks
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.valid?
+      @task.save
+      render json: @task
+    else      
+      render json: { error: "Error creating task", messages: @task.errors.full_messages }, status: :server_error
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    if @task.valid?
+      render json: @task
+    else 
+      render json: { error: "Error updating task", messages: @task.errors.full_messages }, status: :error
+    end
+  end
+
+  def destroy 
+    @task = Task.find(params[:id])
+    @task.destroy
+  end
+
+  private
+  def task_params
+    params.permit(:completed_at, :title)
+  end
 end
