82874c0fac0b5cb46c5925003a32591ce64b9304
Hari Dahal <HDahal@covermymeds.com>
2018-01-22 17:15:47 -0500

    can now create and show new tasks

--- tasks-manager/app/controllers/tasks_controller.rb
+++ tasks-manager/app/controllers/tasks_controller.rb
@@ -1,7 +1,28 @@
 class TasksController < ApplicationController
-  def new
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to tasks_path
+    else
+      render 'new'
+    end
   end
 
   def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :completed)
   end
 end
