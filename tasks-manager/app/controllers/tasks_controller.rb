a7ccb32d085b622536c3ac727480ff8989dd60e9
Hari Dahal <HDahal@covermymeds.com>
2018-01-23 09:01:31 -0500

    delete functionality added

--- tasks-manager/app/controllers/tasks_controller.rb
+++ tasks-manager/app/controllers/tasks_controller.rb
@@ -21,6 +21,13 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
   private
   def task_params
     params.require(:task).permit(:title, :completed)
